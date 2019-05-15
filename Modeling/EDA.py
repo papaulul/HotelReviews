f11c7b05c14ec7a687d40417d009726d75b5d063
papaulul <paulbkim94@gmail.com>
2019-05-14 23:31:43 -0400

    Fixed Price range column

---
+++ Modeling/EDA.py
@@ -0,0 +1,68 @@
+#%% 
+import os
+import pandas as pd
+import numpy as np
+import re
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+#%% 
+data_path = 'files/read_in.pkl'
+reviews = pd.read_pickle(data_path)
+review = reviews.copy()
+#%%
+reviews.describe()
+
+#%%
+count_review = reviews.groupby('hotel_name').agg('count')['review']
+plt.hist(count_review.values, bins = 75)
+plt.title('Histogram of # reviews per hotel')
+
+#%%
+plt.hist(count_review[count_review.values<200].values, bins = 20)
+plt.title('Histogram of # reviews per hotel')
+#%%
+sum(count_review.values < 50)
+
+#%%
+plt.hist(count_review[count_review.values>50].values, bins = 75)
+plt.title('Histogram of # reviews per hotel')
+
+#%%
+count_review[count_review.values <= 50]
+
+#%%
+reviews.groupby('hotel_rating_review').agg('count')['review']
+
+#%%
+for i in reviews.columns[2:]:
+    try : 
+        print(reviews.groupby(i).agg('count')['review'])
+    except:
+        print("Not Compatible for ", i)
+
+#%%
+plt.scatter(reviews.groupby('hotel_rating_hotel').agg('mean')['hotel_rating_review'].index,reviews.groupby('hotel_rating_hotel').agg('mean')['hotel_rating_review'].values)
+plt.title("Average hotel rating by review vs hotel rating in Trip Advisor")
+
+
+#%%
+review['hotel_name'] = reviews['hotel_name'].apply(lambda x: x if x in list(set(count_review[count_review.values > 50].index)) else np.NaN)
+
+
+#%%
+review = review.dropna(axis=0)
+
+#%%
+review.head()
+
+#%%
+plt.scatter(review.groupby('hotel_rating_hotel').agg('mean')['hotel_rating_review'].index,review.groupby('hotel_rating_hotel').agg('mean')['hotel_rating_review'].values)
+plt.title("Average hotel rating by review vs hotel rating in Trip Advisor")
+
+
+#%%
+review.groupby('hotel_rating_hotel').agg('mean')['hotel_rating_review'].values
+
+#%%
---
+++ Modeling/read_in.py
@@ -0,0 +1,89 @@
+#%%
+import os
+import pandas as pd
+import numpy as np
+import pymongo
+from pymongo import MongoClient
+print ('Mongo version', pymongo.__version__)
+client = MongoClient('localhost', 27017)
+db = client.reviews
+reviews = db.full 
+hotel = db.hotel
+import re
+import pickle
+
+#%%
+hotel = pd.DataFrame(list(hotel.find()), columns = ['_id', 'hotelname', 'hotel_rating', 'url', 'num_amenities', 'amenities', 'price_range', 'num_rooms'])
+hotel.rename({'hotelname': 'hotel_name'}, axis=1 ,inplace = True)
+#%%
+reviews = pd.DataFrame(list(reviews.find()), columns = ['_id', 'review', 'hotel_name', 'travel_type', 'bubble', 'url'])
+reviews.rename({'bubble': 'hotel_rating'}, axis=1 ,inplace = True)
+
+#%%
+df = [hotel, reviews]
+
+for dframe in df: 
+    dframe.drop('_id', axis=1, inplace = True)
+    dframe['hotel_name'] = dframe['hotel_name'].apply(lambda x: x[0])
+
+#%%
+# Hotel Preprocessing 
+def review_cleaning(text):
+    # Removes More in list
+    if "More" in text: 
+        text.remove("More")
+    # if list was just More or empty, turns to NA 
+    if len(text) == 0:
+        return np.NaN
+    # Else returns the whole review 
+    else: 
+        return " ".join(text).strip()
+
+reviews['review']=reviews['review'].apply(review_cleaning)
+hotel['hotel_rating_hotel']=hotel['hotel_rating'].apply(lambda x: float(str(x).split(" ")[0]) if x != None else np.NaN)
+reviews['hotel_rating_review']=reviews['hotel_rating'].apply(lambda x: int(str(x).split("_")[-1][0]) if x != None else np.NaN)
+hotel.drop('hotel_rating', axis = 1 , inplace = True)
+reviews.drop('hotel_rating', axis = 1 , inplace = True)
+
+
+#%% 
+hotel['low_price']=hotel['price_range'].apply(lambda x: int(x[0][1:]) if x[0] != "NAN" else np.NaN)
+hotel['high_price']=hotel['price_range'].apply(lambda x: int(x[2][1:].replace(",","")) if x[0] != "NAN" else np.NaN)
+hotel.drop('price_range', axis = 1, inplace = True)
+
+#%%
+# Expanding amenities to individual columns 
+# Will hold all unique amenities
+list_of_all_amenities = []
+# Loop through each row checking to see if any new amenities can be added
+for row in hotel['amenities']:
+    for ele in row: 
+        if ele not in list_of_all_amenities:
+            list_of_all_amenities.append(ele)
+# Sort to make it easier 
+list_of_all_amenities = sorted(list_of_all_amenities)
+# New df that will contain all new columns for amenities 
+versus = hotel[['hotel_name','amenities']]
+# Sort the existing amenities for each hotel
+versus['amenities'] = versus['amenities'].apply(lambda x: sorted(x))
+# Creates new columns for all amenities and set it as false
+for i in list_of_all_amenities:
+    versus[i] = False
+# iterate over each value for amenities and set the index of the amenities to true    
+for ind,value in enumerate(versus['amenities']):
+    for ele in value:
+        versus.set_value(ind,ele, True) 
+# Returns columns back to hotel and removes amenities         
+hotel = hotel.merge(versus.drop('amenities',axis=1), how = 'inner', on ="hotel_name").drop('amenities',axis=1)
+
+#%%
+reviews['hotel_name'].loc[reviews[reviews['hotel_name'] == "Hotel Indigo Atlanta – Vinings"].index]= "Hotel Indigo Atlanta - Vinings"
+reviews['hotel_name'].loc[reviews[reviews['hotel_name'] == 'DoubleTree by Hilton Hotel Atlanta North Druid Hills – Emory Area'].index]= 'DoubleTree by Hilton Hotel Atlanta North Druid Hills - Emory Area'
+#%%
+final = reviews.merge(hotel, how ='left', on= 'hotel_name')
+
+final.to_pickle('files/read_in.pkl')
+
+#%%
+
+#%%
